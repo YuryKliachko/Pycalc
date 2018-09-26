293e3498d842479d62c00da6c853c46ba856c966
Yury Kliachko <uraklechko@gmail.com>
2018-09-26 07:25:02 +0300

    setup.py file created

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pycalc',
+    version='1.0',
+    author='Yury Kliachko',
+    packages=find_packages(),
+    entry_points={'console_scripts': ['pycalc = pycalc.__main__:main']}
+)
